5b32beabe5b314a684a077a1b3ba0d0ca2021a99
devadathkartha <149815290+devadathkartha@users.noreply.github.com>
2024-07-31 22:10:04 +0530

    Add files via upload

---
+++ src/components/About/About.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import CarPng from "../../assets/car1.png";
+
+const About = () => {
+  return (
+    <div className="dark:bg-dark bg-slate-100 sm:min-h-[600px] sm:grid sm:place-items-center duration-300">
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
+          <div data-aos="slide-right" data-aos-duration="1500">
+            <img
+              src={CarPng}
+              alt=""
+              className="sm:scale-125 sm:-translate-x-11 max-h-[300px] drop-shadow-[2px_10px_6px_rgba(0,0,0,0.50)]"
+            />
+          </div>
+          <div>
+            <div className="space-y-5 sm:p-16 pb-6">
+              <h1
+                data-aos="fade-up"
+                className="text-3xl sm:text-4xl font-bold font-serif"
+              >
+                About us
+              </h1>
+              <p data-aos="fade-up" className="leading-8 tracking-wide">
+              Founded in 2015, our car rental company has quickly become a trusted name in the industry, known for delivering exceptional service and countless satisfying experiences. Our commitment to quality and customer satisfaction has earned us a loyal clientele who rely on us for their travel needs. We offer a diverse fleet of well-maintained vehicles, ensuring that our customers enjoy a smooth, reliable, and enjoyable ride every time.
+              </p>
+              
+              <button
+              data-aos="fade-up"
+              data-aos-delay="1500"
+              onClick={() => {
+                AOS.refreshHard();
+              }}
+              className="rounded-md bg-primary hover:bg-primary/80 transition duration-500 py-2 px-6 text-black"
+            >
+              Get Started
+            </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/CarList/CarList.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import whiteCar from "../../assets/white-car.png";
+import car2 from "../../assets/car5.png";
+import car3 from "../../assets/car6.png";
+
+const carList = [
+  {
+    name: "BMW UX",
+    price: 6000,
+    image: whiteCar,
+    aosDelay: "0",
+  },
+  {
+    name: "KIA UX",
+    price: 7999,
+    image: car2,
+    aosDelay: "500",
+  },
+  {
+    name: "BMW UX",
+    price: 10000,
+    image: car3,
+    aosDelay: "1000",
+  },
+];
+
+const CarList = () => {
+  return (
+    <div className="pb-24">
+      <div className="container">
+        {/* Heading */}
+        <h1
+          data-aos="fade-up"
+          className="text-3xl sm:text-4xl font-semibold font-serif mb-3"
+        >
+          Top Rental Cars of Season
+        </h1>
+        <p data-aos="fade-up" aos-delay="400" className="text-sm pb-10">
+        Experience the thrill of driving our top-moving rental cars, where performance and comfort meet your needs.
+        </p>
+        {/* Car listing */}
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16">
+            {carList.map((data) => (
+              <div
+                data-aos="fade-up"
+                data-aos-delay={data.aosDelay}
+                className="space-y-3 border-2 border-gray-300 hover:border-primary p-3 rounded-xl relative group"
+              >
+                <div className="w-full h-[120px]">
+                  <img
+                    src={data.image}
+                    alt=""
+                    className="w-full h-[120px] object-contain sm:translate-x-8 group-hover:sm:translate-x-16 duration-700"
+                  />
+                </div>
+                <div className="space-y-2">
+                  <h1 className="text-primary font-semibold">{data.name}</h1>
+                  <div className="flex justify-between items-center text-xl font-semibold">
+                    <p>₹{data.price}/Day</p>
+                    <a href="#">Details</a>
+                  </div>
+                </div>
+                <p className="text-xl font-semibold absolute top-0 left-3">
+                  12Km
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* End of car listing */}
+        <div className="grid place-items-center mt-8">
+        <button
+              data-aos="fade-up"
+              data-aos-delay="1500"
+              onClick={() => {
+                AOS.refreshHard();
+              }}
+              className="rounded-md bg-primary hover:bg-primary/80 transition duration-500 py-2 px-6 text-black"
+            >
+              Get Started
+            </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarList;
